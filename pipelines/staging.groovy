d219d8bb6713c6e78d491032623b942ffe2bdc25
Paul Karsten <paul.karsten@wipro.com>
2016-10-17 14:15:59 -0600

    Some strange behaviors from shrink-wrap that do not so up on my dev machine.  But adding npm prune seems to help. Oh and try to get rid of those annoying "Using the ‘stage’ step without a block argument is deprecated" messages

--- pipelines/staging.groovy
+++ pipelines/staging.groovy
@@ -3,8 +3,8 @@ node {
     currentBuild.result = "SUCCESS"
 
     try {
-      stage ('Set Up')
-//        sh "curl -L https://dl.bintray.com/buildit/maven/jenkins-pipeline-libraries-${env.PIPELINE_LIBS_VERSION}.zip -o lib.zip && echo 'A' | unzip lib.zip"
+      stage ('Set Up') {
+        sh "curl -L https://dl.bintray.com/buildit/maven/jenkins-pipeline-libraries-${env.PIPELINE_LIBS_VERSION}.zip -o lib.zip && echo 'A' | unzip lib.zip"
 
         ecr = load "lib/ecr.groovy"
         git = load "lib/git.groovy"
@@ -27,38 +27,48 @@ node {
         slackChannel = "synapse"
         gitUrl = "https://bitbucket.org/digitalrigbitbucketteam/eolas"
         appUrl = "http://eolas.staging.${domainName}"
+      }
 
-      stage ('Checkout')
+
+      stage ('Checkout') {
         checkout scm
 
         // global for exception handling
         shortCommitHash = git.getShortCommit()
         def commitMessage = git.getCommitMessage()
         def version = npm.getVersion()
+      }
 
-      stage ('Install')
+      stage ('Install') {
+        sh "rm -rf node_modules"
         sh "npm install"
+      }
 
-      stage ('Validation')
+      stage ('Validation') {
         sh "NODE_ENV='validation' DB_URL='${mongoUrl}' CONTEXT='validation' SERVER_URL='${serverUrl}' SERVER_PORT='${serverPort}' LOG_LEVEL='DEBUG' npm run genConfig"
         sh "NODE_ENV='validation' npm run validate"
+      }
 
-      stage ('Packaging')
+      stage ('Packaging') {
+        sh "NODE_ENV='development' npm prune"
         sh "NODE_ENV='development' npm shrinkwrap"
         sh "NODE_ENV='staging' VERSION='${version}' DB_URL='${mongoUrl}' SERVER_URL='${serverUrl}' SERVER_PORT='${serverPort}' LOG_LEVEL='INFO' npm run package"
         sh "cd dist; npm install --production"
+      }
 
-      stage ('Docker Image Build')
+      stage ('Docker Image Build') {
         def tag = "${version}-${shortCommitHash}-${env.BUILD_NUMBER}"
         def image = docker.build("${appName}:${tag}", '.')
         ecr.authenticate(env.AWS_REGION)
+      }
 
-      stage ('Docker Push')
+      stage ('Docker Push') {
         docker.withRegistry(registry) {
           image.push("${tag}")
         }
+      }
 
-      stage ('Deploy To AWS')
+      stage ('Deploy To AWS') {
         def tmpFile = UUID.randomUUID().toString() + ".tmp"
         def ymlData = template.transform(readFile("docker-compose.yml.template"), [tag: tag, registry_base: registryBase, mongo_url: mongoUrl, db_context: dbContext, server_url: serverUrl, server_port: serverPort])
         writeFile(file: tmpFile, text: ymlData)
@@ -66,18 +76,21 @@ node {
         sh "convox login ${env.CONVOX_RACKNAME} --password ${env.CONVOX_PASSWORD}"
         sh "convox env set NODE_ENV=staging --app ${appName}-staging"
         sh "convox deploy --app ${appName}-staging --description '${tag}' --file ${tmpFile} --wait"
+      }
 
-      stage ('Run Functional Acceptance Tests')
+      stage ('Run Functional Acceptance Tests') {
         // wait until the app is deployed
         convox.waitUntilDeployed("${appName}-staging")
         convox.ensureSecurityGroupSet("${appName}-staging", env.CONVOX_SECURITYGROUP)
         sh "NODE_ENV='acceptance' npm run accept"
+      }
 
-      stage ('Promote Build to latest')
+      stage ('Promote Build to latest') {
         docker.withRegistry(registry) {
           image.push("latest")
         }
         slack.notify("Deployed to Staging", "Commit <${gitUrl}/commits/${shortCommitHash}|${shortCommitHash}> has been deployed to <${appUrl}|${appUrl}>\n\n${commitMessage}", "good", "http://i3.kym-cdn.com/entries/icons/square/000/002/230/42.png", slackChannel)
+      }
     }
     catch (err) {
       currentBuild.result = "FAILURE"
