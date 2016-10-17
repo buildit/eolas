18b94373c662e0cd139a3d36456b1379f5fa7ce4
Paul Karsten <paul.karsten@wipro.com>
2016-10-17 15:20:32 -0600

    Update production pipeline to remove Jenkins pipeline deprivation warnings.

--- pipelines/production.groovy
+++ pipelines/production.groovy
@@ -6,7 +6,7 @@ node {
 
   try {
 
-    stage ('Set Up')
+    stage ('Set Up') {
       checkout scm
 
       sh "curl -L https://dl.bintray.com/buildit/maven/jenkins-pipeline-libraries-${env.PIPELINE_LIBS_VERSION}.zip -o lib.zip && echo 'A' | unzip lib.zip"
@@ -17,28 +17,30 @@ node {
       convox = load "lib/convox.groovy"
       template = load "lib/template.groovy"
 
-      def domainName = "${env.MONGO_HOSTNAME}".substring(8)
-      def appName = "eolas"
-      def registryBase = "006393696278.dkr.ecr.${env.AWS_REGION}.amazonaws.com"
-      def mongoUrl = "mongodb://${env.MONGO_HOSTNAME}:27017"
-      def serverUrl = "${appName}.${domainName}"
-      def serverPort = "80"
-      def context = "production"
+      domainName = "${env.MONGO_HOSTNAME}".substring(8)
+      appName = "eolas"
+      registryBase = "006393696278.dkr.ecr.${env.AWS_REGION}.amazonaws.com"
+      mongoUrl = "mongodb://${env.MONGO_HOSTNAME}:27017"
+      serverUrl = "${appName}.${domainName}"
+      serverPort = "80"
+      context = "production"
 
       // global for exception handling
       slackChannel = "synapse"
       gitUrl = "https://bitbucket.org/digitalrigbitbucketteam/eolas"
       appUrl = "http://eolas.${domainName}"
+    }
 
-    stage ('Write docker-compose')
+    stage ('Write docker-compose') {
       // global for exception handling
-      def tag = "latest"
-      def tmpFile = UUID.randomUUID().toString() + ".tmp"
-      def ymlData = template.transform(readFile("docker-compose.yml.template"), [tag: tag, registry_base: registryBase, mongo_url: mongoUrl, server_url: serverUrl, server_port: serverPort])
+      tag = "latest"
+      tmpFile = UUID.randomUUID().toString() + ".tmp"
+      ymlData = template.transform(readFile("docker-compose.yml.template"), [tag: tag, registry_base: registryBase, mongo_url: mongoUrl, server_url: serverUrl, server_port: serverPort])
 
       writeFile(file: tmpFile, text: ymlData)
+    }
 
-    stage ('Deploy to production')
+    stage ('Deploy to production') {
       sh "convox login ${env.CONVOX_RACKNAME} --password ${env.CONVOX_PASSWORD}"
       sh "convox env set NODE_ENV=production --app ${appName}"
       sh "convox deploy --app ${appName} --description '${tag}' --file ${tmpFile} --wait"
@@ -48,6 +50,7 @@ node {
       convox.waitUntilDeployed("${appName}")
       convox.ensureSecurityGroupSet("${appName}", env.CONVOX_SECURITYGROUP)
       slack.notify("Deployed to Production", "Tag <${gitUrl}/commits/tag/${tag}|${tag}> has been deployed to <${appUrl}|${appUrl}>", "good", "http://i3.kym-cdn.com/entries/icons/square/000/002/230/42.png", slackChannel)
+    }
   }
   catch (err) {
     currentBuild.result = "FAILURE"
