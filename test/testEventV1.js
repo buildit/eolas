d36fcd2d64e55e20b2ec15281956452f40ed3f77
rsafronov <electroma@gmail.com>
2017-01-20 22:44:01 -0500

    Added custom timeout to the failing test

--- test/testEventV1.js
+++ test/testEventV1.js
@@ -149,6 +149,7 @@ describe('Event Listing Services Tests', function() {
   });
 
   it('Test Get Project Event', function(done) {
+    this.timeout(5000);
     var response = buildResponse();
     var request  = HttpMocks.createRequest({
       params: {'name': UNITTESTPROJECT}
